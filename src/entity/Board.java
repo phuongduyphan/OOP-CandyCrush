9be096a569b790d9bff2d800a209717aef16b8cd
lnnchau <nganchau.lenguyen@gmail.com>
2017-10-06 12:56:05 +0700

    Nothing new :)

--- src/entity/Board.java
+++ src/entity/Board.java
@@ -2,64 +2,53 @@ package entity;
 
 import java.util.ArrayList;
 import java.util.Random;
-
 import entity.Candy;
 import tools.Coordinate;
 import tools.Drawer;
 
 
 /**
- * @initialize
- * Initialize the game field:
- * Fill and validate Grid, call Drawer to show the grid
+ * @initialize()
+ * Initialize the game field: 
+ * Fill, validate and show grid
  * 
- * @isValid
- * Check for at least one possible combo
- * i.e player can make at least one more move
+ * @isValid()
+ * Check for at least one possible combo, 
+ * i.e player can make at least one more move.
  * 		
  * @isStable
- * Check for uncrushed combos on the screen
- * Reset the comboList
- * Return true if board is stable
- * Return false and append coors of combos to comboList
+ * Check for uncrushed combos on the screen. 
+ * Return true if board is stable. 
+ * Return false and append coors of combos to comboList. 
  * 
- * @shuffleBoard
- * Shuffle the board when is called
+ * @shuffleBoard()
  * 
- * @fillGrid
- * Fill grid with random candies via calling getRandCandy()
+ * @fillGrid()
+ * Fill grid with random candies.
  * 
  * @swapCandies(Coordinate candy1, Coordinate candy2)
- * Exchange the coordinates of two candies
- * Call drawer.swap(candy1, candy2)
+ * Exchange the coordinates of two candies. 
  * 
  * @crushCandies(ArrayList<Coordinate> comboList)
- * Set null(?) values to candies with coordinates from comboList
- * Call drawer.crush(comboList)
+ * Set null(?) values to candies with coordinates from comboList. 
  * 
- * @moveCandies
- * oldCoorList & newCoorList store positions before and after the move
- * blankList stores positions of unoccupied cells after the move
- * 
- * Append coordinates to oldCoorList, newCoorList and blankList
- * Set new coordinates for the candies and null values for blank cells
- * Call drawer.move(oldCoorList, newCoorList)
- * 
+ * @moveCandies()
+ * Move candies downwards to fill the empty cells after crushing.
+ * Update the positions of candies.
+ *  
  * @newFall(ArrayList<Coordinate> blankList)
- * fallColList stores coordinates for the fall of ONE column
- * newCandyList stores randomly generated candies to fill cells with coor in fallColList
- * ORDER: FROM BOTTOM TO TOP
+ * Fill the unoccupied cells with randomly-generated candies. 
  * 
- * Update fallColList with coordinates with same column from blankList
- * Append newCandyList with candies returned from getRandCandy()
- * Call drawer.newFall(fallColList, newCandyList)
- * 				
+ * @updateBoard()
+ * Update the board after each valid swap until stability is attained.
+ * 
  */
 
 public class Board {
 	static private int NUM_OF_ROWS = 5, NUM_OF_COLS = 5;
 	static private int CELL_HEIGHT = 10, CELL_WIDTH = 10;
 	private ArrayList<Coordinate> comboList, oldCoorList, newCoorList, blankList, fallColList;
+	private ArrayList<Candy> newCandyList;
 	private Drawer drawer;
 	//private CellSelectionHandler cellSelectionHandler;
 	private Candy grid[][];
@@ -91,7 +80,7 @@ public class Board {
 	}
 	
 	public boolean isStable() {
-		comboList.clear();
+		comboList.clear(); //Reset comboList.
 		//TODO
 		//check for stability and modify comboList if necessary
 		
@@ -122,7 +111,7 @@ public class Board {
 	
 	public void swapCandies(Coordinate candy1, Coordinate candy2) {
 		//TODO
-		//update board
+		//update coordinates of two candies
 		
 		drawer.swap(candy1, candy2);
 		
@@ -137,22 +126,45 @@ public class Board {
 	}
 	
 	public void moveCandies() {
-		//TODO
-		//update board
+		/* PARAMETERS:
+			 * oldCoorList & newCoorList store positions before and after the move. 
+			 * blankList stores positions of unoccupied cells after the move. 
+		 
+		 * TODO
+			 * Append coordinates to oldCoorList, newCoorList and blankList. 
+			 * Set new coordinates for the candies and null values for blank cells.
+		 */
 		
 		drawer.move(oldCoorList, newCoorList);
 	}
 	
-	public void newFall() {
-		//TODO
-		//update board
+	public void newFall(ArrayList<Coordinate> blankList) {
+		/* PARAMETERS:
+			 * blankList stores positions of unoccupied cells after the move.
+			 * 
+			 * fallColList stores coordinates for the fall of ONE column. 
+			 * newCandyList stores randomly generated candies to fill cells with coors in fallColList. 
+			 * ORDER: FROM BOTTOM TO TOP.
+		 
+		 * TODO
+		 	* Update fallColList with coordinates with same column from blankList. 
+		 	* Append newCandyList with candies returned from getRandCandy(). 
+		 */
 		
-		drawer.move(oldCoorList, newCoorList);
+		drawer.newFall(fallColList, newCandyList);
 		
 	}
 	
 	public void updateBoard() {
 		//TODO
+		//swap and check:
+		//if swapping doesn't create combo, swap back
+			
+		while (!(isStable())) {
+			crushCandies(comboList);
+			moveCandies();
+			newFall(blankList);
+		}
 	}
 	
 	public void fillGrid() {
